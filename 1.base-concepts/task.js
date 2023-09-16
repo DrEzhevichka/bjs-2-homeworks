b911c61fac6961a5e87809e88eb868d35c8c24ec
DrEzhevichka <eugene.musin@gmail.com>
2023-09-16 16:31:52 +0400

    homeworks base-concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,19 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+  let arr;
+
+  arr = [];
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
+  if (discriminant < 0) {
+    arr;
+  } else if (discriminant === 0) {
+    arr.push(-b / (2 * a));
+  } else {
+    arr.push((-b + Math.sqrt(discriminant)) / (2 * a))
+    arr.push((-b - Math.sqrt(discriminant)) / (2 * a))
+    console.log(arr)
+  }
+  return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
