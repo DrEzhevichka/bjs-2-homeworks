6c55ef3ed2fad27f25b8d33fa4d641aa90569064
DrEzhevichka <eugene.musin@gmail.com>
2023-09-18 18:36:49 +0400

    3.arrays/task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
-  
+    let result = arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
+
+    return result;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter(gndr => gndr.gender === gender).map(ages => ages.age).reduce((acc, item, index, users) => {
+    acc += item;
+    if (index === users.length - 1) {
+      return acc / users.length;
+    }
+    return acc;
+  }, 0);
+  if (users.length === 0) {
+    return 0;
+  }
+  return result;
 }
