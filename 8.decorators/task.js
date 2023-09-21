23b415f3cad0f8fa2dc5f7695affda12ffc3cc7f
DrEzhevichka <eugene.musin@gmail.com>
2023-09-21 18:28:52 +0400

    8.decorators/task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,40 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+    function wrapper(...args) {
+        const hash = md5(args);
+        let objectInCache = cache.find((item) => item.hash === hash);
+        if (objectInCache) {
+            console.log('Из кеша: ' + objectInCache.value);
+            return 'Из кеша: ' + objectInCache.value;
+        }
+        let result = func(...args);
+        cache.push({hash: hash, value: result});
+        if (cache.length > 5) {
+            cache.shift();
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+    }
+    return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    let timeoutId = null;
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    function wrapper(...args) {
+        wrapper.allCount++;
+        if (timeoutId === null) {
+            func.apply(this, [args]);
+            wrapper.count++;
+        }
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            func.apply(this, [args]);
+            wrapper.count++;
+        }, delay);
+    }
+    return wrapper;
 }
